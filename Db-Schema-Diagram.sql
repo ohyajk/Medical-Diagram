d61592759a1a50b0c335a5f2789542a4a7c233e1
ohyajk <ohyajk@gmail.com>
2023-02-28 12:55:23 +0530

    Add foreign key pateint id

--- Db-Schema-Diagram.sql
+++ Db-Schema-Diagram.sql
@@ -7,3 +7,6 @@ CREATE TABLE treatments (id SERIAL PRIMARY KEY, type VARCHAR(20), name VARCHAR(2
 CREATE TABLE invoices (id SERIAL PRIMARY KEY, total_amount DECIMAL, generated_at TIMESTAMP, payed_at TIMESTAMP, medical_history_id INTEGER, FOREIGN KEY (medical_history_id) REFERENCES medical_histories(id));
 
 CREATE TABLE invoice_items (id SERIAL PRIMARY KEY, unit_price DECIMAL, quantity INTEGER, total_price DECIMAL, invoice_id INTEGER, FOREIGN KEY (invoice_id) REFERENCES invoices(id), treatment_id INTEGER);
+
+-- Add Foreign Keys
+ALTER TABLE medical_histories ADD FOREIGN KEY (patient_id) REFERENCES patients(id);
