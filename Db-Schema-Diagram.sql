2724018fae3ab644293e9484a6053a90cb4ec1ab
ohyajk <ohyajk@gmail.com>
2023-02-28 14:07:17 +0530

    join and add foreign key

--- Db-Schema-Diagram.sql
+++ Db-Schema-Diagram.sql
@@ -14,3 +14,11 @@ ALTER TABLE medical_histories ADD FOREIGN KEY (patient_id) REFERENCES patients(i
 ALTER TABLE invoices ADD FOREIGN KEY (medical_history_id) REFERENCES medical_histories(id);
 
 ALTER TABLE invoice_items ADD FOREIGN KEY (treatment_id) REFERENCES treatments(id);
+
+--  Create join table
+create table medical_history_treatments ( medical_history_id int, treatment_id int, primary key(medical_history_id, treatment_id));
+
+--  Add foreign keys to join table
+ALTER TABLE medical_history_treatments ADD FOREIGN KEY (medical_history_id) REFERENCES medical_histories(id);
+
+ALTER TABLE medical_history_treatments ADD FOREIGN KEY (treatment_id) REFERENCES treatments(id);
